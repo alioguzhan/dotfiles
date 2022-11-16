3faaf1cd045ce19c07360d3a9251de4d7a2a0a38
alioguzhan <aoguzhanyildiz@gmail.com>
2022-11-16 21:59:10 +0300

    add starship config

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,15 +3,16 @@
 set -eu -o pipefail # fail on error , debug all lines
 set -x
 
-
 echo "Linking dotfiles to the home folder...."
 
 mkdir -p "$HOME/.zfunc"
+mkdir -p "$HOME/.config"
 
 ln -fs "$PWD/.zshrc" "$HOME/.zshrc"
 ln -fs "$PWD/.vimrc" "$HOME/.vimrc"
 ln -fs "$PWD/.gitconfig" "$HOME/.gitconfig"
 ln -fs "$PWD/.tmux.conf" "$HOME/.tmux.conf"
+ln -fs "$PWD/starship.toml" "$HOME/.config/starship.toml"
 
 touch "$HOME/.env.local" # This is where I keep my secret env variables.
 touch "$HOME/.gitconfig.local"
